75a24fa7cd0b85aea14b6758e2995c16e349df3c
curlicu <curliicu@naver.com>
2018-02-25 12:58:49 +0900

    abc

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//var moment = require('moment');
+import moment from 'moment';
+
+console.log('Hello from javascript');
+console.log(moment().startOf('day').fromNow());
+
+var name = "Hun", time = "toDay";
+console.log(`Hello ${name}, how are you time ${time}`);
